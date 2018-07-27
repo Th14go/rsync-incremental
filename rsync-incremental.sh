7336d0e2aa7b850374da09537460e44c64128273
th14go <thiagoolopezz@gmail.com>
2018-07-27 20:23:38 -0300

    Update rsync-incremental.sh

--- rsync-incremental.sh
+++ rsync-incremental.sh
@@ -1,30 +1,48 @@
 #!/bin/bash
-#Backup Criado por: Thiago Lopes
-#GitHub  - https://github.com/Th14go/rsync-incremental
+#Backup Criado por: Thiago Henrique Lopes
+#GitHub - https://github.com/Th14go/rsync-incremental
+
+#Monta Caminho Backup
+mount /mnt/backup
+
 # PEGA DATA ATUAL, DIA,MES,ANO, HORA,MINUTO,SEGUNDOS, PARA ARQUIVOS DE LOG
-  INICIO=`date +%d/%m/%Y-%H:%M:%S`
+DATA=`date +%x-%k%M%S`
+esp=`df -h /mnt/backup`
+
 # LOCAL PARA ARMAZENAR ARQUIVOS DE LOG
-  LOG=/var/log/rsync/`date +%Y-%m-%d-%H:%M:%S`log-rsync.txt
+LOG=/var/log/rsync/log-$DATA.txt
 
 #  LOCAL ORIGEM PARA BACKUP
-  ORIGEM=/mnt/Dados/ARQUIVOS/ 
+ORIGEM=/home/th14golop35/backup/
 
 # LOCAL DESTINO PARA BACKUP
-  DESTINO=/mnt/backup/INCREMENTAL/
+DESTINO=/mnt/backup/
 
   echo " " >> $LOG
   echo " " >> $LOG
-  echo "| ------ ------ ------ ------ ------ ------ -----------" >> $LOG
-  echo " Sincronizacao iniciada em $INICIO" >> $LOG
-  
-# EXECUTA SCRIPT 
-  rsync --backup --backup-dir=`date +%d-%m-%Y-%R` -Cravzp  $ORIGEM $DESTINO >>$LOG
-  FINAL=`date +%d/%m/%Y-%H:%M:%S`
- 
-  
-  echo " Sincronizacao Finalizada em $FINAL" >> $LOG
-  echo "| ------ ------ ------ ------ ------ ------ -----------" >> $LOG
+  echo "| ----------------------------------------------" >> $LOG
+  echo " Sincronizacao iniciada em $DATA" >> $LOG
+
+# EXECUTA SCRIPT
+
+rsync --backup --backup-dir=$DATA -ravz  --no-perms --no-owner --no-group $ORIGEM $DESTINO >>$LOG
+
+#rsync  -av --no-perms --no-owner --no-group  $ORIGEM $DESTINO >>$LOG
+
+
+  echo " Sincronizacao Finalizada em $DATA " >> $LOG
+  echo "| -----------------------------------------------" >> $LOG
   echo " " >> $LOG
   echo " " >> $LOG
+
+#Verifica Espaço Disponivel em Destino
+echo "-----------------------------------------------------">> $LOG
+echo " Espaço em Disco $esp"  >> $LOG
+echo "-----------------------------------------------------">> $LOG
+
 # ENVIA LOG LOCAL PARA DESTINO REMOTO  DE LOG
-  cp -r /var/log/rsync/ /mnt/backup/LOG
+  cp -r /var/log/rsync/ mnt/backup
+
+
+#Desmonta Caminho Backup
+umount /mnt/backup
