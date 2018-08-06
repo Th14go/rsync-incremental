44b60c6865fd1aa113f9c71c90a53e3604a8edbb
th14go <thiagoolopezz@gmail.com>
2018-08-06 11:54:36 -0300

    Update rsync-incremental.sh

--- rsync-incremental.sh
+++ rsync-incremental.sh
@@ -3,20 +3,20 @@
 #GitHub - https://github.com/Th14go/rsync-incremental
 
 #Monta Caminho Backup
-mount /mnt/backup
+  mount /mnt/backup
 
 # PEGA DATA ATUAL, DIA,MES,ANO, HORA,MINUTO,SEGUNDOS, PARA ARQUIVOS DE LOG
-DATA=`date +%x-%k%M%S`
-esp=`df -h /mnt/backup`
+  DATA=`date +%x-%k%M%S`
+  esp=`df -h /mnt/backup`
 
 # LOCAL PARA ARMAZENAR ARQUIVOS DE LOG
-LOG=/var/log/rsync/log-$DATA.txt
+  LOG=/var/log/rsync/log-$DATA.txt
 
 #  LOCAL ORIGEM PARA BACKUP
-ORIGEM=/home/th14golop35/backup/
+  ORIGEM=/home/user/backup/
 
 # LOCAL DESTINO PARA BACKUP
-DESTINO=/mnt/backup/
+  DESTINO=/mnt/backup/
 
   echo " " >> $LOG
   echo " " >> $LOG
@@ -27,8 +27,6 @@ DESTINO=/mnt/backup/
 
 rsync --backup --backup-dir=$DATA -ravz  --no-perms --no-owner --no-group $ORIGEM $DESTINO >>$LOG
 
-#rsync  -av --no-perms --no-owner --no-group  $ORIGEM $DESTINO >>$LOG
-
 
   echo " Sincronizacao Finalizada em $DATA " >> $LOG
   echo "| -----------------------------------------------" >> $LOG
@@ -36,13 +34,13 @@ rsync --backup --backup-dir=$DATA -ravz  --no-perms --no-owner --no-group $ORIGE
   echo " " >> $LOG
 
 #Verifica Espaço Disponivel em Destino
-echo "-----------------------------------------------------">> $LOG
-echo " Espaço em Disco $esp"  >> $LOG
-echo "-----------------------------------------------------">> $LOG
+  echo "-----------------------------------------------------">> $LOG
+  echo " Espaço em Disco $esp"  >> $LOG
+  echo "-----------------------------------------------------">> $LOG
 
 # ENVIA LOG LOCAL PARA DESTINO REMOTO  DE LOG
   cp -r /var/log/rsync/ mnt/backup
 
 
 #Desmonta Caminho Backup
-umount /mnt/backup
+  umount /mnt/backup
